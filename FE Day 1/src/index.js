995c0f792be909f360df524f7db2400f71b792e0
AlkaffAhamed <alkaffahamed@hotmail.com>
2021-10-28 09:41:21 +0800

    Frontend Day 1 - Used react-marketplace -b doit-10 - Delete button didn't work

---
+++ FE Day 1/src/index.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Marketplace } from "./pages/marketplace";
+import { Career } from "./pages/career"
+
+// ReactDOM.render(<Marketplace />, document.querySelector("#root"));
+ReactDOM.render(<Career />, document.querySelector("#root"));
